420842d0faa03e90e8c9132465bbdb1592eaf61e
Christ122 <chris.tt122@gamil.com>
2014-04-22 21:00:40 -0500

    modified one line

--- tess-two/src/com/googlecode/leptonica/android/AdaptiveMap.java
+++ tess-two/src/com/googlecode/leptonica/android/AdaptiveMap.java
@@ -27,7 +27,7 @@ public class AdaptiveMap {
     }
 // a new line
     // Background normalization constants
-
+// second line
     /** Image reduction value; possible values are 1, 2, 4, 8 */
     private final static int NORM_REDUCTION = 16;
 
